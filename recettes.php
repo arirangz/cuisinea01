0c11cb0aae5a230f5b6c52c124f86e1245539e49
Anthony Laplane <anthony.laplane@gmail.com>
2023-01-24 19:01:45 +0100

    live 5

---
+++ recettes.php
@@ -0,0 +1,22 @@
+<?php
+  require_once('templates/header.php');
+  require_once('lib/recipe.php');
+?>
+
+    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+        <h1>Liste des recettes</h1>
+    </div>
+
+
+    <div class="row">
+
+      <?php foreach ($recipes as $key => $recipe) { 
+        include('templates/recipe_partial.php');
+      } ?>
+
+
+    </div>
+
+<?php
+require_once('templates/footer.php');
+?>
