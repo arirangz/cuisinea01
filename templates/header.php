97cbb9f07ba2945e2a7e9c6cb415445cf6dfb901
Alain BROYE <97102638+ABroye@users.noreply.github.com>
2023-02-22 14:56:13 +0100

    Update header.php
    Add new code into <title></title>

--- templates/header.php
+++ templates/header.php
@@ -15,7 +15,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title><?= $mainMenu[$currentPage]; ?></title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   <link rel="stylesheet" href="assets/css/override-bootstrap.css">
   <link rel="stylesheet" href="assets/css/style.css">
